bb59b0dd3383c20e3e6f0fb9ecb034c2055a57ae
dominiquepieton <pietondominique@yahoo.fr>
2020-01-16 17:55:12 +0100

    add script for the onglet

---
+++ js_onglet/onglet.js
@@ -0,0 +1,74 @@
+(function() {
+    /*
+    *function pour lors d'un click sur onglet 
+    */
+    var afficherOnglet = function (a, animations) {
+        if (animations === undefined) {
+            animations = true
+        }
+        var li = a.parentNode
+        var div = a.parentNode.parentNode.parentNode
+        var activeTab = div.querySelector('.tab-content.active')
+        var aAfficher = div.querySelector(a.getAttribute('href'))
+                // vérification que l'élément contient la class active
+        if(li.classList.contains('active')){
+            return false //permet d'arreter la function
+        }
+                //retire la class de l'élément
+        div.querySelector('.tabs .active').classList.remove('active')
+                // ajout de la class active à l'onglet actuel
+        li.classList.add('active')
+        //retire la class active sur le contenu actif
+        //div.querySelector('.tab-content.active').classList.remove('active')
+        //ajout la class active sur le contenu cliquer
+        //div.querySelector(a.getAttribute('href')).classList.add('active')
+
+            // verification de l'animation
+        if (animations){
+        //on ajoute la class fade sur l'element actif
+            activeTab.classList.add('fade')
+            activeTab.classList.remove('in')
+            var transition = function () {
+                // retire les class fade et active
+                this.classList.remove('fade')
+                this.classList.remove('active')
+            // class fade active element actif
+                aAfficher.classList.add('active')
+                aAfficher.classList.add('fade')
+            // astuce pour un system d'attente
+                aAfficher.offsetWidth
+            // ajout de la class in    
+                aAfficher.classList.add('in')
+            // on casse l'event    
+                activeTab.removeEventListener('transitionend',transition)
+            }
+        //fin de l'animation
+            activeTab.addEventListener('transitionend',transition)
+        } else {
+            aAfficher.classList.add('active')
+            activeTab.classList.remove('active')
+        }
+    }
+    var tabs = document.querySelectorAll('.tabs a')
+
+    for (var i = 0; i < tabs.length; i++){
+        tabs[i].addEventListener('click', function (e) {
+            afficherOnglet(this)
+        })
+    }
+
+    // recuperer le hash
+    var hashChange = function () {
+        var hash = window.location.hash
+        var a = document.querySelector('a[href="' + hash + '"]')
+        //ajout de la class active sur le lien href="hash"
+        console.log(hash)
+        console.log(a)
+        if(a !== null && !a.parentNode.classlist.contains('active')){
+            afficherOnglet(a, false)
+        }
+    }
+    //garder les parametres au retour
+    window.addEventListener('hashchange', hashChange())
+    hashChange()
+})()
